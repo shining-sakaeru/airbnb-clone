c49ec8595ac02859fd616c913fc46352aca7c4ab
sakaeru <heismyst@gmail.com>
2021-06-27 16:35:28 +0900

    ~#8.5 Photo Admin (09:07) updated

--- rooms/models.py
+++ rooms/models.py
@@ -65,7 +65,7 @@ class Photo(core_models.TimeStampedModel):
     """Photo Model Definition"""
 
     caption = models.CharField(max_length=80)
-    file = models.ImageField()
+    file = models.ImageField(upload_to="rooms_photos")
     room = models.ForeignKey("Room", related_name="photos", on_delete=models.CASCADE)
 
     def __str__(self):
