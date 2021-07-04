ec4922d7abdbf1cd0a09abcbca46a649c5c1bbdb
sakaeru <heismyst@gmail.com>
2021-07-04 22:28:57 +0900

    ##11.6 Handling Exceptions (05:01)

--- rooms/views.py
+++ rooms/views.py
@@ -1,12 +1,14 @@
-from math import ceil
-from datetime import datetime
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
+
 
 def all_rooms(request):
     page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)    # 마지막 페이지 element를 이전 페이지에 표시
-    rooms = paginator.page(int(page))
-    return render(request, "rooms/home.html", {"page": rooms})
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms})
+    except EmptyPage:
+        return redirect("/")
