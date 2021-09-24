0dc565e8fc384d8bdfb3c2666bf72ca653f05ee5
sakaeru <heismyst@gmail.com>
2021-09-24 19:07:37 +0900

    #13.9 Forms are Awesome

--- rooms/forms.py
+++ rooms/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django_countries.fields import CountryField
-
 from . import models
 
 
@@ -9,7 +8,7 @@ class SearchForm(forms.Form):
     city = forms.CharField(initial="Anywhere")
     country = CountryField(default="KR").formfield()
     room_type = forms.ModelChoiceField(
-    required=False, empty_label="Any kind", queryset=models.RoomType.objects.all()
+        required=False, empty_label="Any kind", queryset=models.RoomType.objects.all()
     )
     price = forms.IntegerField(required=False)
     guests = forms.IntegerField(required=False)
@@ -19,8 +18,12 @@ class SearchForm(forms.Form):
     instant_book = forms.BooleanField(required=False)
     superhost = forms.BooleanField(required=False)
     amenities = forms.ModelMultipleChoiceField(
-        queryset=models.Amenity.objects.all(), widget=forms.CheckboxSelectMultiple
+        required=False,
+        queryset=models.Amenity.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
     )
     facilities = forms.ModelMultipleChoiceField(
-        queryset=models.Facility.objects.all(), widget=forms.CheckboxSelectMultiple
+        required=False,
+        queryset=models.Facility.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
     )
