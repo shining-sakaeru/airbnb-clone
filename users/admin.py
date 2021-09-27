50d895716c755de6be953b25f2ff20a13d713be3
sakaeru <heismyst@gmail.com>
2021-09-27 22:51:13 +0900

    #17.1 Github Login part Two

--- users/admin.py
+++ users/admin.py
@@ -23,6 +23,7 @@ class CustomUserAdmin(UserAdmin):
                     "language",
                     "currency",
                     "superhost",
+                    "login_method",
                 ),
             },
         ),
@@ -43,4 +44,5 @@ class CustomUserAdmin(UserAdmin):
         "is_superuser",
         "email_verified",
         "email_secret",
+        "login_method",
     )
