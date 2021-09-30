c3e46f84b179612ec542f0dd4367adf6e1d9e466
sakaeru <heismyst@gmail.com>
2021-09-30 21:06:52 +0900

    #19.2 Setup Explanation (09:21)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {}
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  variants: {},
+  plugins: []
 }
