4539bcbd26a2164ded3818fa1ef3825956015261
sakaeru <heismyst@gmail.com>
2021-10-01 18:09:37 +0900

    #20.1 Header part One

--- config/settings.py
+++ config/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/static/"    # server URL
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]    # directory
 
 AUTH_USER_MODEL = "users.User"
 
