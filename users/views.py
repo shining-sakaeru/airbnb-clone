f2a5d23b58af6e95e9752144e33adc195122c869
sakaeru <heismyst@gmail.com>
2021-10-17 14:38:04 +0900

    #21.3 User Profile part One (10:40)

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from django.views.generic import FormView, DetailView
 from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, reverse
@@ -191,3 +192,8 @@ def kakao_callback(request):
     except KakaoException as e:
         messages.error(request, e)
         return redirect(reverse("users:login"))
+
+class UserProfileView(DetailView):
+
+    model = models.User
+    context_object_name = "user_obj"
